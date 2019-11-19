4c6624e17c8ff6b6c2f26b272a3a667870c3af54
Adrian Melian Fdez <alu0100974652@ull.edu.es>
2019-11-19 19:40:59 +0000

    Solucionado la existencia de emisiones de efecto invernadero

--- lib/pract06/pract06.rb
+++ lib/pract06/pract06.rb
@@ -1,10 +1,10 @@
 
 class Alimento
 
-	attr_reader :nombre
+	attr_reader :nombre, :proteinas, :carbohidratos, :lipidos, :gei, :terreno
 
-	def initialize(nombre)
-		@nombre = nombre
+	def initialize(nombre, gei)
+		@nombre, @gei = nombre, gei
 	end
 
 	def giv_nombre(nombre)
