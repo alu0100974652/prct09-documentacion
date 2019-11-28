2b5bc84012972015192636c4e5f2663eed3b3d74
Adrian Melian Fdez <alu0100974652@ull.edu.es>
2019-11-28 16:37:10 +0000

    Creado el initialize de la clase Plato con los atributos nombre y conjunto_alimentos, devuelve correctamente

--- lib/pract06/plato.rb
+++ lib/pract06/plato.rb
@@ -1,11 +1,8 @@
 class Plato
 	attr_accessor :nombre_plato, :conjunto_alimentos
 
-	#def initialize(nombre_plato, conjunto_alimentos)
-	#	@nombre_plato, @conjunto_alimentos = nombre_plato, conjunto_alimentos
-	#end
+	def initialize(nombre_plato, conjunto_alimentos)
+		@nombre_plato, @conjunto_alimentos = nombre_plato, conjunto_alimentos
+	end
 
-	#def giv_nombre(nombre_plato)
-	#	return @nombre_plato
-	#end
 end
